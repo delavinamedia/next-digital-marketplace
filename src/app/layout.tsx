88d1cd6677d03163fae5c9c59304e0f58865e14c
delavinamedia <koeppen.h@gmail.com>
2024-04-06 00:04:37 +0200

    metadata enhanced

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,30 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { cn, constructMetadata } from "@/lib/utils";
+import { cn } from "@/lib/utils"; // constructMetadata 
 import NavBar from "@/components/NavBar";
 import Providers from "@/components/providers/Providers";
 import { Toaster } from "sonner";
 import Footer from "@/components/Footer";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = constructMetadata();
+// export const metadata = constructMetadata();
+export const metadata: Metadata = {
+  title: {
+    template: '%s | digiprops',
+    default: 'digiprops by delavina',
+  },
+  description: 'beautifully designed digital props for filmmaking',
+  icons: {
+    icon: "/favicon.ico", 
+  },
+  keywords: ['props', 'Requisiten', 'filmmaking', 'Grafik'],
+  publisher: 'Holger Köppen | delavina',
+  openGraph: {
+    images: '/og-image.png',
+  },
+}
 
 // TODO: add dark team implementation
 
@@ -20,7 +36,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body
-        className={cn("relative h-full font-sans antialiased", inter.className)}
+        className={cn(
+          "relative h-full font-sans antialiased", 
+          inter.className
+        )}
       >
         <main className="relative flex flex-col min-h-screen">
           <Providers>
